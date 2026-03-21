3733d30463982d5d9eb73060e82f1a9745ec527d
sametd <sametdemir@gmail.com>
2026-03-21 18:31:17 +0100

    perf: use Arc<Value> in WorkResponse and restore field doc comments

--- bits/src/dispatcher/executor/remote_pool.rs
+++ bits/src/dispatcher/executor/remote_pool.rs
@@ -146,8 +146,8 @@ fn default_timeout_ms() -> u64 {
 struct WorkResponse {
     job_id: String,
     request: serde_json::Value,
-    user: serde_json::Value,
-    metadata: serde_json::Value,
+    user: std::sync::Arc<serde_json::Value>,
+    metadata: std::sync::Arc<serde_json::Value>,
 }
 
 #[derive(Deserialize)]
@@ -287,8 +287,8 @@ async fn handle_get_work(
     let resp = WorkResponse {
         job_id: job.id.clone(),
         request: job.request.clone(),
-        user: (*job.user).clone(),
-        metadata: (*job.metadata).clone(),
+        user: job.user.clone(),
+        metadata: job.metadata.clone(),
     };
 
     Ok(Json(resp))

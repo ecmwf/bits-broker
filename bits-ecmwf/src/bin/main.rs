f424bca30465938480a5e7d1efd6c74d87e87504
sametd <sametdemir@gmail.com>
2026-03-28 02:05:31 +0100

    refactor: extract shared shutdown_signal() to bits::server, restore API docs

--- bits-ecmwf/src/bin/main.rs
+++ bits-ecmwf/src/bin/main.rs
@@ -25,29 +25,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (bits, server_config) = bits::parse_bootstrap(&config_str)?.into_parts()?;
     let bits = Arc::new(bits);
 
-    bits::server::serve_with_shutdown(bits, server_config, shutdown_signal()).await
-}
-
-async fn shutdown_signal() {
-    let ctrl_c = tokio::signal::ctrl_c();
-    #[cfg(unix)]
-    {
-        match tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()) {
-            Ok(mut sigterm) => {
-                tokio::select! {
-                    _ = ctrl_c => {}
-                    _ = sigterm.recv() => {}
-                }
-            }
-            Err(e) => {
-                tracing::warn!(error = %e, "SIGTERM registration failed, falling back to Ctrl-C");
-                ctrl_c.await.ok();
-            }
-        }
-    }
-    #[cfg(not(unix))]
-    {
-        ctrl_c.await.ok();
-    }
-    tracing::info!("shutdown signal received, draining");
+    bits::server::serve_with_shutdown(bits, server_config, bits::server::shutdown_signal()).await
 }
